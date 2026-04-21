92a7421ab2b2b71ba2468660348a2297a55c8dd8
Nilesh Patil <nileshbhilapatil123@gmail.com>
2026-04-21 05:44:56 +0530

    get_data file added and upadate

---
+++ get_data.py
@@ -0,0 +1,38 @@
+import sys
+import os 
+import sys
+import json 
+
+from dotenv import load_dotenv
+import certifi
+
+import pandas as pd 
+import numpy as np 
+import pymongo
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+
+class NetworkDataExtraction():
+    def __int__(self):
+        try:
+            pass
+        except Exception as e :
+            raise NetworkSecurityException(e,sys)
+
+
+    def csv_tojson_convertor(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def pushing_data_to_mongodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+if __name__ == "__main__":
+    pass
