307f3c408e0836dd913a5dbea93cefe79c8f3f7a
Nilesh Patil <nileshbhilapatil123@gmail.com>
2026-04-25 17:43:19 +0530

    data transformation done

--- networksecurity/pipelines/traininig_pipeline.py
+++ networksecurity/pipelines/traininig_pipeline.py
@@ -91,9 +91,9 @@ class TrainingPipeline():
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             print(data_ingestion_artifact)
-            data_ingestion_artifact  = self.start_data_validation(data_ingestion_artifact)
-            print(data_ingestion_artifact)
-            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+            print(data_validation_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
             
