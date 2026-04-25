0013fa667a0ed7aeef6fbe871b51684a7886099f
Nilesh Patil <nileshbhilapatil123@gmail.com>
2026-04-25 17:27:49 +0530

    utils is update

--- networksecurity/pipelines/traininig_pipeline.py
+++ networksecurity/pipelines/traininig_pipeline.py
@@ -58,9 +58,15 @@ class TrainingPipeline():
             raise NetworkSecurityException(e,sys)
         
 
-    def start_data_transformation(self):
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
-            pass
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config)
+            
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
@@ -85,7 +91,9 @@ class TrainingPipeline():
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             print(data_ingestion_artifact)
-            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+            data_ingestion_artifact  = self.start_data_validation(data_ingestion_artifact)
+            print(data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
             
